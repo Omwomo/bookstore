72ac58a7b712c1954bb4a30a810f5af0b9ee57cb
Omwomo <eugeneoduor79@gmail.com>
2023-09-05 17:37:22 +0300

    Add reducers to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import bookSlice from './books/bookSlice';
+import categoriesSlice from './categories/categoriesSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    book: bookSlice,
+    categories: categoriesSlice,
+  },
 });
 
 export default store;
