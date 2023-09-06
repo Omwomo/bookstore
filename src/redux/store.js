02db75291e9269dcca112842b546e0661a5e9199
Omwomo <eugeneoduor79@gmail.com>
2023-09-06 09:19:54 +0300

    Dispatch actions of displaying the books and remove book button

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import categoriesSlice from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
-    book: bookSlice,
+    books: bookSlice,
     categories: categoriesSlice,
   },
 });
