9e5e94bca2c1ed6836a7b07696d73927d87031f9
Omwomo <eugeneoduor79@gmail.com>
2023-09-07 13:24:32 +0300

    Removed hard coded book lists

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -36,26 +36,7 @@ export const removeBookAsync = createAsyncThunk('books/removeBook', async (bookI
 });
 
 const initialState = {
-  books: [
-    {
-      id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  books: [],
 };
 
 const bookSlice = createSlice({
